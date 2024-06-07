297b04827585cdf68be9b15f8902b3fbca94818d
DmitryMolokanov <molod1690@yandex.ru>
2024-06-07 07:41:14 +0300

    fitrst commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { IWord } from "./types";
 
 function App() {
+  const [arrWord, setArrWord] = useState(["box", "word", "live", "strong"]);
+  const [word, setWord] = useState("");
+  const [wordAudio, setWordAudio] = useState("");
+  const [wordIndex, setWordIndex] = useState(0);
+
+  const getWord = () => {
+    let index = 0;
+    let dIndex = 0;
+    let description: string[] = [];
+    setInterval(async () => {
+      // проверка, что индекс не больше массива
+      if (index < arrWord.length) {
+        // получение данных по API
+        const response = await fetch(
+          `https://api.dictionaryapi.dev/api/v2/entries/en/${arrWord[index]}`
+        );
+        const result: IWord[] = await response.json();
+        console.log(result);
+        // установить слово, которое будет отображено
+        setWord(result[0].word);
+        // проерить и установить озвучку слова
+        if (result[0].phonetics[2]) {
+          setWordAudio(result[0].phonetics[2].audio);
+        } else setWordAudio(result[0].phonetics[1].audio);
+        // следующая итерация
+
+        result[0].meanings[0].definitions.forEach;
+
+        console.log(description);
+        index++;
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    console.log(wordIndex);
+  }, [wordIndex]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Word</h4>
+      <div>{word}</div>
+      <audio src={wordAudio} autoPlay />
+      <h4>Definitions</h4>
+      <div>{}</div>
+      <button onClick={getWord}>start</button>
+      <button>stop</button>
+      <button>restart</button>
     </div>
   );
 }
---
+++ src/types.ts
@@ -0,0 +1,21 @@
+export interface IWord {
+  license: { name: string; url: string };
+  meanings: Array<IMeanings>;
+  phonetic: string;
+  phonetics: { text: string; audio: string }[];
+  sourceUrls: string;
+  word: string;
+}
+
+export interface IMeanings {
+  antonyms: Array<string>;
+  definitions: [
+    {
+      antonyms: Array<string>;
+      definition: string;
+      synonyms: Array<string>;
+    }
+  ];
+  partOfSpeech: string;
+  synonyms: Array<string>;
+}
