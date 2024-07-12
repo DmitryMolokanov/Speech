447d9dfa48e0c73937e650510be778cf89fb227b
PULSAR\molokanov <molokanov@pulsarm.ru>
2024-07-12 15:40:17 +0300

    try fixed Chrome play bug

--- src/pages/VoiceRecord.tsx
+++ src/pages/VoiceRecord.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header';
 import { Button, Box } from '@mui/material';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
 const VoiceRecord = () => {
+    const [value, setValue] = useState('I lov english')
     const {
         transcript,
         listening,
@@ -21,8 +22,10 @@ const VoiceRecord = () => {
             <Header />
             <p>Microphone: {listening ? 'on' : 'off'}</p>
             <Box width={360} height={300} sx={{ border: '2px solid' }}>{transcript}</Box>
-            <Button onPointerDown={() => SpeechRecognition.startListening()}>Start</Button>
+            <Button onPointerDown={() => SpeechRecognition.startListening({ continuous: true, language: 'en-US' })}>Start</Button>
             <Button onPointerDown={() => SpeechRecognition.stopListening()}>Stop</Button>
+
+            <textarea spellCheck lang='en' value={value}></textarea>
         </div>
     )
 };
